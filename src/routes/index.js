8f0692ad74109bb1f5b76327ad36c1b482f9c799
Jhon Carlos Colorado <8047140+jcolorado19@users.noreply.github.com>
2020-02-12 22:01:50 -0500

    Crear rutas del sitio

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import Header from '../templates/Header'
+import Home from '../page/Home'
+import Character from '../page/Character'
+import Error404 from '../page/Error404'
+
+// Se establecen las rutas que va a tener la aplicacion
+const routes = {
+    '/': Home,
+    '/:id': Character,
+    '/Contact': 'Contact',
+}
+
+
+// Vamos a crear el manejador el cual se va a encargar de mostrar los elementos segun la logica obtenida
+const router = async () => {
+    const header = null || document.getElementById('header')
+    const content = null || document.getElementById('content')
+
+    header.innerHTML = await Header()
+}
+
+export default router
