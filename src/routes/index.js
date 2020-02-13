2e73322ee35bb94d15bc01479c7b58cd5a3c7627
Jhon Carlos Colorado <8047140+jcolorado19@users.noreply.github.com>
2020-02-13 05:14:07 -0500

    Implementar y probar las conexiones

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,15 @@ import Header from '../templates/Header'
 import Home from '../page/Home'
 import Character from '../page/Character'
 import Error404 from '../page/Error404'
-import getHast from '../utils/getHash'
+import getHash from '../utils/getHash'
 import resolveRoutes from '../utils/resolveRoutes'
 
 // Se establecen las rutas que va a tener la aplicacion
 const routes = {
     '/': Home,
     '/:id': Character,
-    '/Contact': 'Contact',
-}
+    '/contact': 'Contact',
+};
 
 
 // Vamos a crear el manejador el cual se va a encargar de mostrar los elementos segun la logica obtenida
@@ -19,6 +19,11 @@ const router = async () => {
     const content = null || document.getElementById('content')
 
     header.innerHTML = await Header()
+
+    let hash = getHash()
+    let route = await resolveRoutes(hash)
+    let render = routes[route] ? routes[route] : Error404
+    content.innerHTML = await render()
 }
 
 export default router
